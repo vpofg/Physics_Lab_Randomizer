a1a9531d60914c61db19835d5978f77fca32a990
Jan Gwara <janekgwara13@gmail.com>
2024-10-31 10:58:44 +0100

    finished the randomizer

--- randomOhm.py
+++ randomOhm.py
@@ -1,16 +1,15 @@
 import random 
-from random import randrange
 
 
 
 def ohmRand():
-    ohmVal = float(input("Enter the ohm value: "))
-    toleranceRes = float(input("Enter the tolerance value in percent: "))
+    ohmVal = input("Enter the ohm value: ")
+    toleranceRes = input("Enter the tolerance value in percent: ")
     toleranceRange = (float(toleranceRes) / 100) * float(ohmVal)
-    lowerBound = ohmVal - toleranceRange
-    upperBound = ohmVal + toleranceRange
+    lowerBound = float(ohmVal) - float(toleranceRange)
+    upperBound = float(ohmVal) + float(toleranceRange)
     for i in range (0, 5):
-        randNum = randrange(lowerBound, upperBound)
+        randNum = random.uniform(lowerBound, upperBound)
         print(randNum)
 
 
